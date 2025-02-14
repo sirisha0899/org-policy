71e2977f0fd8414658d254a72476c17a608f4a18
sirisha0899 <134088388+sirisha0899@users.noreply.github.com>
2025-02-14 13:31:19 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "org_id" {
+  description = "Your Google Cloud Organization ID"
+  type        = string
+}
+
+variable "project_id" {
+  description = "The project ID where resources will be managed"
+  type        = string
+}
